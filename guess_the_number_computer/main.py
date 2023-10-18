0c4c8d8abcc9e54c4f20f50dcf3e975131bd923d
grmbyrn <graemebyrne_89@hotmail.com>
2023-10-18 10:55:50 +0100

    add computer guess using while loop, conditionals and random to guess-the-number project

--- guess_the_number_computer/main.py
+++ guess_the_number_computer/main.py
@@ -1,5 +1,6 @@
 import random
 
+
 def guess(x):
     random_number = random.randint(1, x)
     guess = 0
@@ -10,7 +11,29 @@ def guess(x):
             print('Sorry, guess again. Too low.')
         elif guess > random_number:
             print('Sorry, guess again. Too high.')
-        
+
     print('Correct')
 
-guess(10)
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low  # could also be high b/c low = high
+        feedback = input(
+            f"Is {guess} too high (H), too low (L), or correct (C)?").lower()
+
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess - 1
+
+    print(f'The computer guessed your number, {guess}, correctly!')
+
+
+computer_guess(1000)
